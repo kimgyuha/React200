e2d68bff31621c4f503bd97fc8d19ea4dd35bcbc
KGH <sideway360@gmail.com>
2023-08-21 15:51:16 +0900

    vs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
+
 import React from 'react';
-//import ImportFuncComponent from './Components/ImportFuncComponent';
 import './App.css';
-//import LifecylceEx from './R004_LifecycleEx';
-//import ImportComponent from './R003_ImportComponent'
-//import LifecylceEx from "./Components/LifecycleExFunc";
-//import LifecylceEx from "./R005_LifecycleEx";
-import LifecycleExFunc2 from './Components/LifecycleExFunc2';
+import LifecycleEx from './R008_LifecycleEx';
 
 const App = () => {
   return(
     <div>
       <h1>Start React200!</h1>
       <p>CSS 적용하기</p>
-      <LifecycleExFunc2 />
+      <LifecycleEx pValue="from.js" />
     </div>
   );
 };
---
+++ src/Components/LifecycleExFuncThird.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LifecycleExFuncThird = (props) => {
+    console.log(props);
+    return <h2>This is a component that receives a [{props.pValue}]</h2>;
+};
+export default LifecycleExFuncThird;
